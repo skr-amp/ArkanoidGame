52f5ad4bd40d3b9ea295b6bf55457bcc9ae0975d
skramp <skramp@mail.ru>
2023-11-30 13:42:12 +0500

    Добавлен GameOver

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -22,9 +23,16 @@ public class Main extends Application {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
-    int score = 0;
-    int lives = 3;
-    boolean gameIsStop = true;
+    int score;
+    int lives;
+    boolean gameIsStop;
+    boolean gameIsOver;
+    Ball ball;
+    Platform platform;
+    Set<Brick> bricks;
+
+    Text livesView;
+    Text scoreView;
 
     public static void main(String[] args)  {
         launch(args);
@@ -34,37 +42,29 @@ public class Main extends Application {
     public void start(Stage stage) {
         Pane pane = new Pane();
         Scene scene = new Scene(pane, WIDTH, HEIGHT);
+        Pane gameOverPane = new Pane();
+        Scene gameOverScene = new Scene(gameOverPane, WIDTH, HEIGHT);
+        Text gameOverText = new Text();
+        gameOverText.setX(150);
+        gameOverText.setY(250);
+        gameOverText.setFont(new Font(40));
+        gameOverText.setTextAlignment(TextAlignment.CENTER);
+        gameOverText.setText("Вы проиграли:(\nДля начала новой игры\nнажмите ENTER");
+        gameOverPane.getChildren().add(gameOverText);
 
         stage.setScene(scene);
         stage.setTitle("Arkanoid");
         stage.setResizable(false);
+        initGame(pane);
         stage.show();
-        Text scoreView = new Text();
-        scoreView.setX(620);
-        scoreView.setY(580);
-        scoreView.setFont(new Font(30));
-        scoreView.setText("Очки: " + score);
-        pane.getChildren().add(scoreView);
-        Text livesView = new Text();
-        livesView.setX(30);
-        livesView.setY(580);
-        livesView.setFont(new Font(30));
-        livesView.setText("Жизни: " + lives);
-        pane.getChildren().add(livesView);
 
-        Ball ball = new Ball(400, 510, pane);
-        Platform platform = new Platform(pane);
-        Set<Brick> bricks = new HashSet<>();
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 4; j++){
-                bricks.add(new Brick(80 * i + 50, 40 * j + 20, pane));
-            }
-        }
-        System.out.println(bricks);
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(5), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+                if(gameIsOver){
+                    stage.setScene(gameOverScene);
+                }
                 collideCheck(ball, platform, bricks);
                 ball.move();
                 scoreView.setText("Очки: " + score);
@@ -93,8 +93,57 @@ public class Main extends Application {
                     if (gameIsStop){ball.setDy(-1.2);}
                     gameIsStop = false;
                 }
+                else if (keyEvent.getCode() == KeyCode.ENTER) {
+                    gameIsOver = false;
+                    gameIsStop = true;
+                    System.out.println("Enter");
+                    stage.setScene(scene);
+                }
             }
         });
+
+        gameOverScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    gameIsOver = false;
+                    gameIsStop = true;
+                    initGame(pane);
+                    stage.setScene(scene);
+                }
+            }
+        });
+    }
+
+    public void initGame(Pane pane){
+        gameIsStop = true;
+        gameIsOver = false;
+        score = 0;
+        lives = 1;
+        pane.getChildren().clear();
+        scoreView = new Text();
+        scoreView.setX(620);
+        scoreView.setY(580);
+        scoreView.setFont(new Font(30));
+        scoreView.setText("Очки: " + score);
+        pane.getChildren().add(scoreView);
+        livesView = new Text();
+        livesView.setX(30);
+        livesView.setY(580);
+        livesView.setFont(new Font(30));
+        livesView.setText("Жизни: " + lives);
+        pane.getChildren().add(livesView);
+
+        ball = new Ball(400, 510, pane);
+
+        platform = new Platform(pane);
+
+        bricks = new HashSet<>();
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 4; j++){
+                bricks.add(new Brick(80 * i + 50, 40 * j + 20, pane));
+            }
+        }
     }
 
     public void collideCheck(Ball ball, Platform platform, Set bricks){
@@ -107,6 +156,9 @@ public class Main extends Application {
         if((ball.getY() > 550) && ((ball.getX() < platform.getX()) || (ball.getX() > platform.getX() + 140))){
             gameIsStop = true;
             lives--;
+            if(lives == 0){
+                gameIsOver = true;
+            }
             ball.setDy(0);
             ball.moveTo(platform.getX() + 70, 510);
         }
@@ -134,6 +186,4 @@ public class Main extends Application {
                 }
         }
     }
-
-
 }
